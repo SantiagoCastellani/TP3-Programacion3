08c2b4d709e5ff2946d3f053d8a582010098d595
SantiagoCastellani <monitocastellani@gmail.com>
2024-04-23 00:00:08 -0300

    Se agrego textBox Usuario

--- TP3_Grupo5/TP3_Grupo5/ejercicio1.aspx.designer.cs
+++ TP3_Grupo5/TP3_Grupo5/ejercicio1.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace TP3_Grupo5
         protected global::System.Web.UI.WebControls.Button btnSelectLocalidad;
 
         /// <summary>
+        /// Control txtNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreUsuario;
+
+        /// <summary>
         /// Control ddlLocalidades.
         /// </summary>
         /// <remarks>
